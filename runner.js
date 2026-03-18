db2ed32ae862b0aa50ed9ce3204d2d8299e55f9c
Nodir Xakimov <nodirsayfullayevich@gmail.com>
2026-03-18 12:23:37 +0500

    Data added and version updated

--- runner.js
+++ runner.js
@@ -1,11 +1,9 @@
 import fs from "fs"
 
-const base = "https://openbudget.uz/api/v2/info/votes/69b9af132f5aa943b6a7c8e8"
+const base = "https://openbudget.uz/api/v2/info/votes/69ba52a23d01cb2096e035d1"
 
 async function getAll(){
-
   let all=[]
-
   const firstRes = await fetch(`${base}?page=0`,{
     headers:{
       "Accept":"application/json",
@@ -13,36 +11,23 @@ async function getAll(){
       "Referer":"https://openbudget.uz/"
     }
   })
-
   const firstText = await firstRes.text()
   const first = JSON.parse(firstText)
-
   all.push(...first.content)
-
   const totalPages = first.totalPages
-
   for(let page=1; page<=20; page++){  // page<=totalPages
-
     const res = await fetch(`${base}?page=${page}`)
     const text = await res.text()
-
     if(!text){
       console.log("Empty page",page)
       continue
     }
-
     const data = JSON.parse(text)
-
     all.push(...data.content)
-
     console.log(`Page ${page}/${totalPages}`)
-
   }
-
   fs.writeFileSync("votes.json",JSON.stringify(all,null,2))
-
   console.log("Saved",all.length)
-
 }
 
 getAll()
